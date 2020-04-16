07e2f7266e5afc6e055f56db5e499a748eb1565d
NguyenHoangAnhTien <63226589+NguyenHoangAnhTien@users.noreply.github.com>
2020-04-17 00:06:36 +0700

    version 1.2

--- src/nguyenhoanganhtien_59136132_baitap1/QLDS.java
+++ src/nguyenhoanganhtien_59136132_baitap1/QLDS.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 /**
  *
- * @author ltpnt
+ * @author PC
  */
 public class QLDS implements IQLDS{
     private final List<CaNhan> CaNhan = new ArrayList<>();
